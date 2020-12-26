15d8363183b55b07814bc5d136f4caf062fb21e4
Choi-Yong-Yeol <88dydfuf@naver.com>
2020-12-27 03:34:37 +0900

    Event Handling

--- hello-react/hello-react/src/App.js
+++ hello-react/hello-react/src/App.js
@@ -1,33 +1,10 @@
 import React, { Component, Fragment} from 'react';
-import './App.css';
-import MyComponent from './MyComponent'
+import EventPractice from './EventPractice';
 
 class App extends Component{
   render(){
-    const text = "당신은 어썸한가요?";
-    const condition = true;
-    const style = {
-      backgroundColor : 'gray',
-      border : '1px solid black',
-      height : Math.round(Math.random() * 300) + 50,
-      width : Math.round(Math.random() * 300) + 50,
-      WebkitTransition : 'all',
-      MozTransition : 'all',
-      msTransition : 'all'
-    };
     return (
-      <div className="my-div">
-        <MyComponent name="alskd"/>
-        <h2>리액트 안녕!</h2>
-        <h2>당신은 어썸한가요?</h2>
-        <h1>{text}</h1>
-        {
-          condition ? 'dd' : '거짓'
-        }
-        <div 
-        style = {style}></div>
-      </div>
-
+      <EventPractice/>
     );
   }
 }
