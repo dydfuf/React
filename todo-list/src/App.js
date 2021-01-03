6b282c877fc1c342250b9e4efd8e0a1be4a2dfd8
Choi-Yong-Yeol <88dydfuf@naver.com>
2021-01-04 02:47:28 +0900

    버전 문제로 잠시 중단

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,25 +1,15 @@
+import React, { Component} from 'react';
+import PageTemplate from './component/PageTemplate'
 import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  render(){
+    return (
+      <PageTemplate>안녕하세요</PageTemplate>
+    );
+  }
 }
 
 export default App;
