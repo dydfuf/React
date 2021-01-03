0096abcf7d92c5334f6400663680e517a764ae2e
Choi-Yong-Yeol <88dydfuf@naver.com>
2021-01-03 19:55:27 +0900

    styling-react

--- hello-react/hello-react/src/App.js
+++ hello-react/hello-react/src/App.js
@@ -1,28 +1,20 @@
-import React, { Component, Fragment} from 'react';
-import LifeCycleSample from './LifeCycleSample';
+import React, { Component} from 'react';
+import logo from './logo.svg';
+import './App.css'
 
-function getRandomColor(){
-  return '#' + Math.floor(Math.random() * 16777215).toString(16);
-}
 
 class App extends Component{
-  state = {
-    color: '#000000'
-  }
-
-  handleClick = () => {
-    this.setState({
-      color: getRandomColor()
-    })
-  }
-
   render(){
     return (
-      <div>
-      <button onClick={this.handleClick}>랜덤 색상</button>
-      <LifeCycleSample color={this.state.color}/>
+      <div className="App">
+        <div ClassName="app-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
-
     );
   }
 }
